194bf2cd743c4c9d7a6c0642241c8b6beba2b0ef
matheus-06 <matheus2006.pinheiro@gmail.com>
2026-06-01 13:45:34 -0300

    refactor(firmware): Formtação automatica nos codigos do labirinto

--- src/firmware/lib/labirinto.h
+++ src/firmware/lib/labirinto.h
@@ -2,16 +2,18 @@
 #include <Arduino.h>
 
 #define LAB_TAM 31
-#define DIST_INFINITA 32767 // Distancia maxima é 31*31 
+#define DIST_INFINITA 32767 // Distancia maxima é 31*31
 
-typedef struct Celula {
+typedef struct Celula
+{
     short distancia;               // valor do flood fill (max 960 para 31×31)
     bool norte, sul, leste, oeste; // paredes conhecidas
     bool explorado;                // robô já visitou
-    bool visitado;                 // visitou para o DFS 
+    bool visitado;                 // visitou para o DFS
 } Celula;
 
-typedef struct Labirinto {
+typedef struct Labirinto
+{
     Celula celula[LAB_TAM][LAB_TAM];
 } Labirinto;
 
